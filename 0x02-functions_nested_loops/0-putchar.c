8f0ca024e61d08e777030a084c8aa0085a081bd2
Oposibu <abrahamakewukereke@gmail.com>
2022-09-13 22:09:01 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "abraham.h"
+
+/**
+ *main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+
+	_putchar ('A');
+	_putchar ('B');
+	_putchar ('R');
+	_putchar ('A');
+	_putchar ('H');
+	_putchar ('A');
+	_putchar ('M');
+	_putchar ('\n');
+	return (0);
+
+}
+
+
