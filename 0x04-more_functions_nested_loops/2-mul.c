423a9d3a5ece646f2e0ba9cc73a935a53f8e1c30
Oposibu <abrahamakewukereke@gmail.com>
2022-09-15 13:22:06 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- *_isdigit - checks whether a  character is a digit or not
- *@c: tested character
- *Return: 1 if it is, 0 otherwise
- */
+*mul - multiplies two numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
+
 int mul(int a, int b)
 {
 	return (a * b);
