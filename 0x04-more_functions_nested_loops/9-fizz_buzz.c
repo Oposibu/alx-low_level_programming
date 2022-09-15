d4037389d13824510f4467fc1b2de7d98529012e
Oposibu <abrahamakewukereke@gmail.com>
2022-09-15 14:43:34 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,11 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - program that prints either number
- * or fizz or buzz or fizzBuzz
+ *main - program that prints either number
+ *or fizz or buzz or fizzBuzz
  *
  *
- * Return: returns 0
+ *Return: returns 0
  */
 
 int main(void)
