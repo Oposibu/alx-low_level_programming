62a35d8a0d7206ca60112ec9f54c2b361413bc2e
Oposibu <abrahamakewukereke@gmail.com>
2022-09-27 09:26:46 -0700

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 	 unsigned int i;
 
 	 /*Declaring FOR*/
-	 for (i = 0; i < n; i++)
-	 {
-		 *(s + i) = b; /*add 1 position s*/
-
-	 } /*END FOR*/
-	 
-	 return (s);
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+	} /*END FOR*/
+	return (s);
 }
